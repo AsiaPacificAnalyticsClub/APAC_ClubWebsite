4c13b48d6f931311912ce852ce728d045d6df3fe
LohYenKuan <lohyenkuan@gmail.com>
2025-02-10 14:22:01 +0800

    Fix another error about the event page not fetching the latest data

--- app/api/getAllData/route.js
+++ app/api/getAllData/route.js
@@ -1,7 +1,5 @@
 import { MongoClient } from "mongodb";
 
-export const revalidate = 0;
-
 export async function GET() {
   const client = new MongoClient(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -14,11 +12,22 @@ export async function GET() {
     const collection = database.collection("events");
     const allData = await collection.find({}).toArray();
 
-    return new Response(JSON.stringify(allData), { status: 200 });
+    return new Response(JSON.stringify(allData), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store, max-age=0",
+        "Surrogate-Control": "no-store",
+      },
+    });
   } catch (error) {
     console.error(error);
     return new Response(JSON.stringify({ message: "Something went wrong!" }), {
       status: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store, max-age=0",
+      },
     });
   } finally {
     await client.close();
