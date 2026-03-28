e23e92abe3c48872cf68f97f6dc545a20224b79f
MingCyn <joslyn.cyn05@gmail.com>
2026-03-28 20:48:22 +0800

    add google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   description: "Asia Pacific Analytics Club Website",
   icons: "/apaclogo.png",
   verification: {
-    google: "<meta name="google-site-verification" content="i1aubWiItRKhg9Y4R6qSIXm2dgzpGDpYyCXuRx5NXz4" />", // Paste your full code here
+    google: "i1aubWiItRKhg9Y4R6qSIXm2dgzpGDpYyCXuRx5NXz4", 
   },
 };
 
