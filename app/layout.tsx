a80183f512cabfb026d055224442502fea25f2d0
Kishanea <Kishanea@LAPTOP-FD3QNK9N>
2026-05-06 22:04:51 +0800

    UI change for club showcase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans , Sora } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { twMerge } from "tailwind-merge";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
+const sora = Sora({ subsets: ["latin"], variable: "--font-sora" });
 export const metadata: Metadata = {
   title: "APAC",
   description: "Asia Pacific Analytics Club Website",
@@ -19,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={twMerge(dmSans.className, "antialiased bg-white")}>
+      <body className={twMerge(dmSans.className, sora.variable, "antialiased bg-white")}>
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
