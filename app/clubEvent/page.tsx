698f5d23788f580ebe2233675416b27f05b2b1a3
Kishanea <Kishanea@LAPTOP-FD3QNK9N>
2026-05-24 19:26:46 +0800

    Countdown

--- app/clubEvent/page.tsx
+++ app/clubEvent/page.tsx
@@ -14,6 +14,9 @@ import Image from "next/image";
 import { Event } from "@/constants/Events";
 import { ApiEvent } from "@/constants/ApiEvent";
 import { images } from "@/constants/Images";
+import EventCountdown from "./eventCountdown";
+//temporary winner banner
+import WinnerBanner from "@/components/WinnerBanner";
 
 const ITEMS_PER_PAGE = 10;
 
@@ -348,6 +351,7 @@ const ClubEvent = () => {
 
   return (
     <div className="min-h-screen w-full">
+      <EventCountdown />
       <div className="mb-8 pt-8">
         <h1 className="text-3xl font-bold text-center mb-2">Club Events</h1>
         <p className="text-center text-gray-600">
