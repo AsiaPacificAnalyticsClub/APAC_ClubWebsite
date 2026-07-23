1fd0016df4f7540816fdc4ef846556430e7b0ac3
Teh Wen Chieh <wenchiehteh@gmail.com>
2026-07-23 11:46:48 +0800

    remove unused imports

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Facebook, Instagram, Mail, MapPin } from "lucide-react";
+import { Instagram, Mail, MapPin } from "lucide-react";
 import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
